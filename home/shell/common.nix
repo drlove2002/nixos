b28e3021ffd49e0a420579249eea1104004eea9c
Sudip Roy <sudiproy20yo@gmail.com>
2025-12-03 00:26:14 +0530

    feat: Add common shell aliases and session variables

--- home/shell/common.nix
+++ home/shell/common.nix
@@ -16,4 +16,23 @@
     nix-direnv.enable = true;
     enableZshIntegration = true;
   };
+
+  home.shellAliases = {
+    copy = "xclip -selection clipboard";
+    paste = "xclip -selection clipboard -o";
+    nixbuild = "sudo nixos-rebuild switch --upgrade";
+    nixgc = "sudo nix-collect-garbage";
+    nixrmold = "sudo nix-env --delete-generations old";
+    htop = "btop";
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    BROWSER = "zen-browser";
+    TERMINAL = "kitty";
+    DELTA_PAGER = "less -R";
+    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    WINEPREFIX = "${config.xdg.dataHome}/wine";
+  };
 }
