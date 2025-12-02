cfb7e65ebaa1df3b34fd60240f409356ddd587d9
Sudip Roy <sudiproy20yo@gmail.com>
2025-12-03 00:26:15 +0530

    feat: Configure PostgreSQL authentication

--- hosts/pc/db.nix
+++ hosts/pc/db.nix
@@ -4,6 +4,16 @@
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_17;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser origin-address auth-method
+      local all      all     trust
+      # ... other auth rules ...
+
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host  all      all     ::1/128        trust
+    '';
   };
 
   services.redis = {
