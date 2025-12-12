8ea26116c9fa779ad5371b395141923bb922902c
Sudip Roy <sudiproy20yo@gmail.com>
2025-12-12 23:15:29 +0530

    refactor: Update kitty configs

--- home/shell/terminal.nix
+++ home/shell/terminal.nix
@@ -9,6 +9,14 @@
     settings = {
       enable_audio_bell = false;
       shell = "zsh";
+      cursor_trail = 3;
+      cursor_trail_decay = "0.1 0.4";
+      background_opacity = 0.5;
+      scrollback_lines = 10000;
+      scrollback_pager_history_size = 10;
+      mouse_hide_wait = 3.0;
+      hide_window_decorations = "yes";
+      notify_on_cmd_finish invisible = 20;
     };
     themeFile = "kanagawa_dragon";
   };
