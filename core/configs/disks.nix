d2422ede91a9ee01ef528fc676f8284a00af1e32
Sudip Roy <sudiproy20yo@gmail.com>
2026-01-11 09:58:34 +0530

    refactor: Handle shared mount at boot gracefully

--- core/configs/disks.nix
+++ core/configs/disks.nix
@@ -3,10 +3,13 @@
     device = "/dev/disk/by-uuid/1039142B1039142B";
     fsType = "ntfs3"; # Modern NTFS driver (or "ntfs-3g" for older systems)
     options = [
-      "rw"
-      "user"
-      "noauto"
-      "x-systemd.automount"
+      "defaults"
+      "nofail"
+      "force" # If dirty flag persists post-chkdsk
+      "uid=1000" # Your user ID (run `id -u`)
+      "gid=100" # Your GID or shared group
+      "dmask=011" # Dirs: drwxrw-rw-rw
+      "fmask=011" # Files: -rw-rw-rw-
     ];
   };
   swapDevices = [
