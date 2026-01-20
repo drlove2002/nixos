56f330b626f4b37a8d132206c4d928e382aabf5f
Sudip Roy <sudiproy20yo@gmail.com>
2026-01-20 13:37:16 +0530

    feat(scripts): Add direct symlink for edit without rebuild

--- home/shell/scripts/default.nix
+++ home/shell/scripts/default.nix
@@ -1,10 +1,11 @@
-{...}: {
-  # Source scripts from the home-manager store
-  home.file = {
-    ".local/bin" = {
-      recursive = true;
-      source = ./bin;
-    };
+{
+  config,
+  lib,
+  ...
+}: {
+  home.file."${config.home.homeDirectory}/.local/bin" = {
+    recursive = true;
+    source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nixos/home/shell/scripts/bin");
   };
   home.sessionPath = [
     "$HOME/.local/bin"
