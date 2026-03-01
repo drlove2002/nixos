ad9dbab40a23ff3f87c54d0803eb6e60c00369a6
Sudip Roy <sudiproy20yo@gmail.com>
2026-03-01 11:14:55 +0530

    refactor(nvim): Disable Mason and refine Rust LSP config

--- home/programs/nvim/configs/lua/plugins/rust.lua
+++ home/programs/nvim/configs/lua/plugins/rust.lua
@@ -1,10 +1,43 @@
+-- ~/.config/nvim/lua/plugins/rust.lua
 return {
   {
     "mrcjkb/rustaceanvim",
+    version = "^5",
     ft = { "rust" },
     opts = {
-      dap = {}, -- leave empty; rustaceanvim will use nvim-dap if available
-      -- other rustaceanvim options...
+      server = {
+        on_attach = function(client, bufnr)
+          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end,
+        default_settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            checkOnSave = true,
+            check = {
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+              workspace = true, -- ← Scan entire workspace
+            },
+            -- Enable workspace diagnostics
+            diagnostics = {
+              enable = true,
+              experimental = {
+                enable = true,
+              },
+            },
+            procMacro = {
+              enable = true,
+            },
+          },
+        },
+      },
+      dap = {},
     },
   },
   { "saecki/crates.nvim", ft = "toml", opts = {} },
