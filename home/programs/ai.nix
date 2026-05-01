1628ee33a8380380291157737f393c4b40139407
Sudip Roy <sudiproy20yo@gmail.com>
2026-05-01 15:55:14 +0530

    Revert "chore: Override pi to use local dev clone"
    This reverts commit df9c3021b25da952bfcc7f1210a66615ef41bccd.

--- home/programs/ai.nix
+++ home/programs/ai.nix
@@ -8,16 +8,6 @@ in {
   home.packages = [
     llmAgents.codex
     llmAgents.opencode
-    # llmAgents.pi # We run this locally from ~/Projects/pi-mono
+    llmAgents.pi
   ];
-
-  # Make a wrapper script so 'pi' runs from your local clone
-  home.file.".local/bin/pi" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-      cd ~/Projects/pi-mono/packages/coding-agent
-      exec npx tsx src/index.ts "$@"
-    '';
-  };
 }
