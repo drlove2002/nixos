eace0f178d15addb5a003759230f607527d2af95
b <b>
2026-06-30 01:11:31 +0530

    bisect: shell sub ./shell/zsh.nix

---
+++ home/darwin-bisect.nix
@@ -0,0 +1,20 @@
+{
+  inputs, username, lib, config, ...
+}: {
+  disabledModules = [
+    "targets/darwin/fonts.nix"
+    "targets/darwin/linkapps.nix"
+    "targets/darwin/copyapps.nix"
+  ];
+  imports = [
+    ./dummy-options.nix
+    ./shell/aliases.nix
+    ./shell/zsh.nix
+  ];
+  home = {
+    username = username;
+    homeDirectory = lib.mkForce "/Users/${username}";
+    stateVersion = "25.05";
+  };
+  programs.home-manager.enable = true;
+}
