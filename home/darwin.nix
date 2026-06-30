f34170cca6ec1663758a9676fc8b29d85f7ccce6
pi <pi>
2026-06-30 10:57:32 +0530

    fix: symlink apps into ~/Applications directly, not subdir

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,5 +1,7 @@
 { inputs, username, lib, config, ... }: {
   disabledModules = [
+    "targets/darwin/linkapps.nix"
+    "targets/darwin/copyapps.nix"
   ];
 
   imports = [
@@ -17,4 +19,19 @@
   };
 
   programs.home-manager.enable = true;
+
+  # Symlink .app bundles into ~/Applications (flat, no subdir)
+  home.activation.linkApplications = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    apps_dir="${config.home.homeDirectory}/Applications"
+    hm_apps="${config.home.path}/Applications"
+    rm -rf "$apps_dir"
+    mkdir -p "$apps_dir"
+    if [ -d "$hm_apps" ]; then
+      for app in "$hm_apps"/*.app; do
+        [ -e "$app" ] || continue
+        name="$(basename "$app")"
+        ln -sfn "$app" "$apps_dir/$name"
+      done
+    fi
+  '';
 }
